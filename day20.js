b354ec18ae7ca6cf603a55a4c50aeb79564e4400
Rishita-chawla <146315995+Rishita-chawla@users.noreply.github.com>
2024-02-17 18:44:49 +0530

    Update day20.js

--- day20.js
+++ day20.js
@@ -1,11 +1,26 @@
-const url="https://api.openweathermap.org/data/2.5/weather?q=austin&appid=e5ca505642e54ff56f3c09e68dca8812";
-//fetching the weather of austin (a city in US)
+const url="https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=e5ca505642e54ff56f3c09e68dca8812";
+
 fetch(url).then((res)=>{
     console.log("RESOLVED!",res);
     return res.json();
 }).then((data)=>{
+    const {name, weather, main}=data
     console.log("LOADING THE FETCHED DATA!");
     console.log(data);
+    const cityName = document.createElement("h2");
+    cityName.innerText = `Weather in ${name}`;
+    document.body.appendChild(cityName);
+    const listItems=[`Weather Description:${weather[0].description}`,
+    `temp:${main.temp}`,`temp_min:${main.temp_min}`,
+    `temp_max:${main.temp_max}`,`pressure:${main.pressure}`,`humidity:${main.humidity}`];
+    const myList=document.createElement("ul");
+    listItems.forEach(item=>{
+        const content=document.createElement("li");
+        content.innerText=item;
+        myList.appendChild(content)
+    });
+    document.body.appendChild(myList);
+    
 }).catch((e)=>{
     console.log("OOPSSS AN ERRO",e);
 });
